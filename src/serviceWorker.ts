20315e4e372fe55f2526844267d1828ce3d72694
inamesh <22492259+inamesh@users.noreply.github.com>
2020-03-29 12:50:35 +0300

    localhost definition in serviceworker

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -17,7 +17,8 @@ const isLocalhost = Boolean(
     // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+    ) ||
+    window.location.hostname === '192.168.2.39'
 );
 
 type Config = {
